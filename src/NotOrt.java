2ff6be71f6008143407001bc8f5487e063819677
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-04-22 14:45:55 +0300

    not hesaplama

---
+++ src/NotOrt.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class NotOrt {
+    public static void main(String[] args) {
+        // Değişkenleri oluştur
+        int mat, fizik, kimya,turkce,tarih,muzik;
+
+        //Scanner sınıfımızı tanımladık
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanıcıdan değerleri al
+        System.out.print("Matematik Notunuz : ");
+        mat =inp.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik =inp.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya =inp.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce =inp.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih =inp.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik =inp.nextInt();
+
+        int toplam =(mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc =toplam/6.0;
+        System.out.println("Ortalamanız : " + sonuc);
+
+        //Ortalama notu
+        if (sonuc > 60){
+            System.out.println("Sınıfı Geçtiniz Tebrikler :)");
+        }else {
+        System.out.println("Sınıfı Geçemediniz :(");
+        }
+    }
+}
