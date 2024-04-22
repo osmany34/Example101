adbd362a2718f5c5a73c80686c1cfe9338459601
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-04-22 16:44:17 +0300

    Taksimetre hesaplama

--- src/Week1/Taksimetre.java
+++ src/Week1/Taksimetre.java
@@ -1,14 +1,15 @@
 package Week1;
 
-import java.nio.file.LinkPermission;
 import java.util.Scanner;
 
 public class Taksimetre {
     public static void main(String[] args) {
 
+        //Değişken tanımlama
         int km;
         double perKm = 2.20, total, startPrice = 10;
 
+        // Kullanıcı verileri
         Scanner input = new Scanner(System.in);
         System.out.println("Mesafeyi KM cinsinden giriniz : ");
         km = input.nextInt();
