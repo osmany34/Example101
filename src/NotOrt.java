07b15b830ed52ecc1749f878505ad0dd6eaa3146
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-04-22 14:52:56 +0300

    not hesaplama

--- src/NotOrt.java
+++ src/NotOrt.java
@@ -3,39 +3,36 @@ import java.util.Scanner;
 public class NotOrt {
     public static void main(String[] args) {
         // Değişkenleri oluştur
-        int mat, fizik, kimya,turkce,tarih,muzik;
+        int mat, fizik, kimya, turkce, tarih, muzik;
 
         //Scanner sınıfımızı tanımladık
         Scanner inp = new Scanner(System.in);
 
         //Kullanıcıdan değerleri al
         System.out.print("Matematik Notunuz : ");
-        mat =inp.nextInt();
+        mat = inp.nextInt();
 
         System.out.print("Fizik Notunuz : ");
-        fizik =inp.nextInt();
+        fizik = inp.nextInt();
 
         System.out.print("Kimya Notunuz : ");
-        kimya =inp.nextInt();
+        kimya = inp.nextInt();
 
         System.out.print("Türkçe Notunuz : ");
-        turkce =inp.nextInt();
+        turkce = inp.nextInt();
 
         System.out.print("Tarih Notunuz : ");
-        tarih =inp.nextInt();
+        tarih = inp.nextInt();
 
         System.out.print("Müzik Notunuz : ");
-        muzik =inp.nextInt();
+        muzik = inp.nextInt();
 
-        int toplam =(mat + fizik + kimya + turkce + tarih + muzik);
-        double sonuc =toplam/6.0;
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6.0;
         System.out.println("Ortalamanız : " + sonuc);
 
-        //Ortalama notu
-        if (sonuc > 60){
-            System.out.println("Sınıfı Geçtiniz Tebrikler :)");
-        }else {
-        System.out.println("Sınıfı Geçemediniz :(");
-        }
+        // Ortalama
+        String sonucMesaji = sonuc > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(sonucMesaji);
     }
 }
