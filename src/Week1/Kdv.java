86ab267fabb8754980683ded30961eb0c66e093c
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-04-22 15:43:25 +0300

    Kdv hesaplama uygulaması yapımı

---
+++ src/Week1/Kdv.java
@@ -0,0 +1,32 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args) {
+        double tutar, kdvOran = 0.18, kdvTutar, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz : ");
+
+        tutar = input.nextDouble();
+        kdvTutar =tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        // KDV oranı belirleme
+        if (tutar <= 1000) {
+            // Tutar 1000 TL veya daha azsa %18 KDV
+            kdvOran = 0.18;
+        } else {
+            // Tutar 1000 TL'den büyükse %8 KDV
+            kdvOran = 0.08;
+        }
+
+        System.out.println("KDV'siz Tutar :" + tutar);
+        System.out.println("KDV Oranı :" + kdvOran);
+        System.out.println("KDV Tutarı :" + kdvTutar);
+        System.out.println("KDV'li Tutarı :" + kdvliTutar);
+
+
+    }
+}
