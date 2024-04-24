2b3a2f1eb212d32c4eb87dc5bcef95c3542a0406
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-04-24 15:31:01 +0300

    Hesap makinesi uygulaması

--- src/Week1/HesapMakinesi.java
+++ src/Week1/HesapMakinesi.java
@@ -4,36 +4,40 @@ import java.util.Scanner;
 
 public class HesapMakinesi {
     public static void main(String[] args) {
-        int n1, n2, select;
+        int number_1, number_2, select;
 
         Scanner input = new Scanner(System.in);
         System.out.print("İlk Sayıyı Giriniz : ");
-        n1 = input.nextInt();
+        number_1 = input.nextInt();
 
         System.out.print("İkinci Sayıyı Giriniz : ");
-        n2 = input.nextInt();
+        number_2 = input.nextInt();
 
+        // Kullanıcıya hangi işlemleri seçebileceği mesaj
         System.out.println("1-Toplama \n2-Çıkarma \n3-Çarpma \n4-Bölme");
         System.out.print("Seçiminiz : ");
         select =input.nextInt();
 
+        // Seçime göre switch-case yapısı
         switch (select){
             case 1:
-                System.out.println("Toplama İşlemi Sonucu : " + (n1+n2));
+                System.out.println("Toplama İşlemi Sonucu : " + (number_1 + number_2));
                 break;
             case 2 :
-                System.out.println("Çıkarma İŞlemi Sonucu : " + (n1-n2));
+                System.out.println("Çıkarma İŞlemi Sonucu : " + (number_1 - number_2));
                 break;
             case 3 :
-                System.out.println("Çarpma İşlemi Sonucu : " + (n1*n2));
+                System.out.println("Çarpma İşlemi Sonucu : " + (number_1 * number_2));
                 break;
+            // Bölme işlemi (sıfıra bölme durumu)
             case 4 :
-                if (n2 !=0){
-                    System.out.println("Bölme İşlemi Sonucu : " + (double) n1/n2);
+                if (number_2 !=0){
+                    System.out.println("Bölme İşlemi Sonucu : " + (double) number_1 / number_2);
                 }else {
                     System.out.println("Hatalı İşlem ! ");
                 }
                 break;
+            // 1 ve 4 arasında seçim almazsa
             default:
                 System.out.println("!!!!!! Geçersiz Seçim !!!!! ");
 
