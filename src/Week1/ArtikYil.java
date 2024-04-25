f237509e7c35a5ca81b67fe322584a191179d985
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-04-25 12:24:54 +0300

    Artık Yıl Hesaplama

---
+++ src/Week1/ArtikYil.java
@@ -0,0 +1,20 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Kullanıcıdan yıl bilgisi alma
+        System.out.print("Yıl Giriniz : ");
+        int yil = input.nextInt();
+
+        // Alınan yıla göre artık olup olmadığı hesaplanan döngü
+        if ((yil % 4 == 0 && yil % 100 != 0) || (yil % 400 == 0)){
+            System.out.println(yil + " bir artık yıldır.");
+        }else {
+            System.out.println(yil + " bir arık yıl değildir !");
+        }
+    }
+}
