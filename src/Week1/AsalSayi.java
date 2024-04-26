714c5eb311b313c00c3b18bd155f0e8c1cc1be34
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-04-26 19:12:23 +0300

    1 - 100 arasındaki asal sayıları ekrana yazdıran program

--- src/Week1/AsalSayi.java
+++ src/Week1/AsalSayi.java
@@ -1,7 +1,4 @@
 package Week1;
-
-import java.util.Scanner;
-
 public class AsalSayi {
     public static void main(String[] args) {
         System.out.println("1 ile 100 arasındaki asal sayılar : ");
