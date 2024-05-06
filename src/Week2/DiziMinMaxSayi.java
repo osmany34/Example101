f9a7942ef39b8096bc74a7536c53ea750446ecf3
osmany34 <127791332+osmany34@users.noreply.github.com>
2024-05-06 13:56:03 +0300

    Dizideki elemanların girilen sayıdan küçük en yakın sayıyı ve en büyük en yakın sayıyı bulan program

---
+++ src/Week2/DiziMinMaxSayi.java
@@ -0,0 +1,35 @@
+package Week2;
+
+import java.util.Arrays;
+
+public class DiziMinMaxSayi {
+    public static void main(String[] args) {
+        int[] number = {10, 9, 22, 1, 93, -6, -53, 0};
+        int inputNumber = 21;
+
+        Arrays.sort(number);
+
+        int numberSmaller = Integer.MIN_VALUE;
+        for (int num : number) {
+            if (num < inputNumber) {
+                numberSmaller = num;
+            } else {
+                break;
+            }
+        }
+
+        int numberGreater = Integer.MAX_VALUE;
+        for (int i = number.length - 1; i >= 0; i--) {
+            if (number[i] > inputNumber) {
+                numberGreater = number[i];
+            } else {
+                break;
+            }
+        }
+        System.out.println("Dizi : " + java.util.Arrays.toString(number));
+        System.out.println("Girilen Sayı : " + inputNumber);
+        System.out.println("Girilen sayıdan küçük en yakın sayı : " + numberSmaller);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : " + numberGreater);
+
+    }
+}
